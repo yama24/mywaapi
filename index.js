cf2e3143a489d9c9fce4b7ead85a991b83c38af7
Yayan Maulana <maulana24@live.com>
2022-04-16 09:44:16 +0700

    socket.io

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // const qrcode = require("qrcode-terminal");
-const qrcode = require('qrcode');
+const qrcode = require("qrcode");
 const express = require("express");
 const http = require("http");
 const fileUpload = require("express-fileupload");
 const axios = require("axios");
-const socketIO = require('socket.io');
+const socketIO = require("socket.io");
 const { phoneNumberFormatter } = require("./formatter");
 const { body, validationResult } = require("express-validator");
 const {
@@ -13,6 +13,7 @@ const {
   MessageMedia,
   List,
   Buttons,
+  ClientInfo,
 } = require("whatsapp-web.js");
 
 const port = process.env.PORT || 8000;
@@ -63,6 +64,10 @@ client.initialize();
 
 io.on("connection", function (socket) {
   socket.emit("message", "Connecting...");
+  
+  client.on("change_state", (state) => {
+    socket.emit("message", `State changed to ${state}`);
+  });
 
   client.on("qr", (qr) => {
     console.log("QR RECEIVED", qr);
@@ -83,15 +88,15 @@ io.on("connection", function (socket) {
     console.log("AUTHENTICATED");
   });
 
-//   client.on("auth_failure", function (session) {
-//     socket.emit("message", "Auth failure, restarting...");
-//   });
+  client.on("auth_failure", function (session) {
+    socket.emit("message", "Auth failure, restarting...");
+  });
 
-//   client.on("disconnected", (reason) => {
-//     socket.emit("message", "Whatsapp is disconnected!");
-//     client.destroy();
-//     client.initialize();
-//   });
+  client.on("disconnected", (reason) => {
+    socket.emit("message", "Whatsapp is disconnected!");
+    client.destroy();
+    client.initialize();
+  });
 });
 
 // client.on("qr", (qr) => {
@@ -107,67 +112,67 @@ io.on("connection", function (socket) {
 //   console.log("Client is ready!");
 // });
 
-// client.on("message", async (message) => {
-//   if (message.body === "!ping") {
-//     message.reply("pong"); // mode reply
-//   } else if (message.body.startsWith("!sendto ")) {
-//     // Direct send a new message to specific id
-//     var number = message.body.split(" ")[1];
-//     var messageIndex = message.body.indexOf(number) + number.length;
-//     var messageBody = message.body.slice(messageIndex, message.body.length);
-//     number = number.includes("@c.us") ? number : `${number}@c.us`;
-//     client.sendMessage(number, messageBody);
-//     message.reply("message sent to " + number);
-//   } else if (message.body === "!info") {
-//     let info = client.info;
-//     client.sendMessage(
-//       message.from,
-//       `
-//         *Connection info*
-//         User name: ${info.pushname}
-//         My number: ${info.wid.user}
-//         Platform: ${info.platform}
-//     `
-//     );
-//     //   } else if (message.body === "!buttons") {
-//     //     var button = new Buttons(
-//     //       "Button body",
-//     //       [{ body: "bt1" }, { body: "bt2" }, { body: "bt3" }],
-//     //       "title",
-//     //       "footer"
-//     //     );
-//     //     client.sendMessage(message.from, button);
-//     //   } else if (message.body === "!lists") {
-//     //     client.sendMessage(message.from, "lists");
-//     //     var sections = [
-//     //       {
-//     //         title: "sectionTitle",
-//     //         rows: [
-//     //           { title: "ListItem1", description: "desc" },
-//     //           { title: "ListItem2" },
-//     //         ],
-//     //       },
-//     //     ];
-//     //     var list = new List("List body", "btnText", sections, "Title", "footer");
-//     //     client.sendMessage(message.from, list);
-//   } else if (message.body == "!groups") {
-//     client.getChats().then((chats) => {
-//       const groups = chats.filter((chat) => chat.isGroup);
+client.on("message", async (message) => {
+  if (message.body === "!ping") {
+    message.reply("pong"); // mode reply
+  } else if (message.body.startsWith("!sendto ")) {
+    // Direct send a new message to specific id
+    var number = message.body.split(" ")[1];
+    var messageIndex = message.body.indexOf(number) + number.length;
+    var messageBody = message.body.slice(messageIndex, message.body.length);
+    number = number.includes("@c.us") ? number : `${number}@c.us`;
+    client.sendMessage(number, messageBody);
+    message.reply("message sent to " + number);
+  } else if (message.body === "!info") {
+    let info = client.info;
+    client.sendMessage(
+      message.from,
+      `
+        *Connection info*
+        User name: ${info.pushname}
+        My number: ${info.wid.user}
+        Platform: ${info.platform}
+    `
+    );
+    //   } else if (message.body === "!buttons") {
+    //     var button = new Buttons(
+    //       "Button body",
+    //       [{ body: "bt1" }, { body: "bt2" }, { body: "bt3" }],
+    //       "title",
+    //       "footer"
+    //     );
+    //     client.sendMessage(message.from, button);
+    //   } else if (message.body === "!lists") {
+    //     client.sendMessage(message.from, "lists");
+    //     var sections = [
+    //       {
+    //         title: "sectionTitle",
+    //         rows: [
+    //           { title: "ListItem1", description: "desc" },
+    //           { title: "ListItem2" },
+    //         ],
+    //       },
+    //     ];
+    //     var list = new List("List body", "btnText", sections, "Title", "footer");
+    //     client.sendMessage(message.from, list);
+  } else if (message.body == "!groups") {
+    client.getChats().then((chats) => {
+      const groups = chats.filter((chat) => chat.isGroup);
 
-//       if (groups.length == 0) {
-//         message.reply("You have no group yet.");
-//       } else {
-//         let replyMsg = "*YOUR GROUPS*\n\n";
-//         groups.forEach((group, i) => {
-//           replyMsg += `ID: ${group.id._serialized}\nName: ${group.name}\n\n`;
-//         });
-//         replyMsg +=
-//           "_You can use the group id to send a message to the group._";
-//         message.reply(replyMsg);
-//       }
-//     });
-//   }
-// });
+      if (groups.length == 0) {
+        message.reply("You have no group yet.");
+      } else {
+        let replyMsg = "*YOUR GROUPS*\n\n";
+        groups.forEach((group, i) => {
+          replyMsg += `ID: ${group.id._serialized}\nName: ${group.name}\n\n`;
+        });
+        replyMsg +=
+          "_You can use the group id to send a message to the group._";
+        message.reply(replyMsg);
+      }
+    });
+  }
+});
 
 const checkRegisteredNumber = async function (number) {
   const isRegistered = await client.isRegisteredUser(number);
